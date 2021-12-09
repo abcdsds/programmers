1d1499ff821b2ef831f5bf6ffb109a4b1646bf61
abcdsds <choiyurim2@naver.com>
2021-12-09 09:30:15 +0900

    refactor(mockExam): change map to int array

--- src/level1/mockExam.java
+++ src/level1/mockExam.java
@@ -2,15 +2,12 @@ package level1;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class mockExam {
 
     public static int[] solution(int[] answers) {
-        Map<Integer, Integer> a = new HashMap<>();
-        a.put(1, 0);
-        a.put(2, 0);
-        a.put(3, 0);
-
+        int[] a = {0, 0, 0};
         int[] ints = {1, 2, 3, 4, 5};
         int[] ints1 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] ints2 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
@@ -18,33 +15,24 @@ public class mockExam {
         for (int i = 0; i < answers.length; i++) {
             int answer = answers[i];
             if (answer == ints[i % 5]) {
-                Integer integer = a.get(1);
-                a.put(1, integer + 1);
+                a[0] = a[0] + 1;
             }
 
             if (answer == ints1[i % 8]) {
-                Integer integer = a.get(2);
-                a.put(2, integer + 1);
+                a[1] = a[1] + 1;
             }
             if (answer == ints2[i % 10]) {
-                Integer integer = a.get(3);
-                a.put(3, integer + 1);
+                a[2] = a[2] + 1;
             }
         }
 
-
-        Map.Entry<Integer, Integer> integerIntegerEntry = a.entrySet()
-                .stream()
-                .max(
-                        Comparator.comparing(
-                                Map.Entry::getValue))
-                .get();
-
-        int[] answer = a.entrySet()
-                .stream()
-                .filter(
-                        e -> integerIntegerEntry.getValue() == e.getValue())
-                .mapToInt(mm -> mm.getKey())
+        int max = Arrays.stream(a)
+                .max()
+                .getAsInt();
+        int[] answer = IntStream.range(0, a.length)
+                .filter(i -> a[i] == max)
+                .map(i -> i+1)
+                .sorted()
                 .toArray();
 
         return answer;
